244d6be496339c905473603376cc4f7cc3280daf
zpvoh <16302010023@fudan.edu.cn>
2018-08-15 23:15:32 +0800

    json structure complete

--- out/artifacts/GloomyWebServer_war_exploded/index.jsp
+++ out/artifacts/GloomyWebServer_war_exploded/index.jsp
@@ -7,10 +7,18 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.0/mini-default.min.css">
+    <script src="https://cdn.bootcss.com/react/16.4.0/umd/react.development.js"></script>
+    <script src="https://cdn.bootcss.com/react-dom/16.4.0/umd/react-dom.development.js"></script>
+    <script src="https://cdn.bootcss.com/babel-standalone/6.26.0/babel.min.js"></script>
+    <script src="https://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/babel" src="js/react_render.js"></script>
+    <script src="js/getContent.js"></script>
+</head>
+<body>
+
+</body>
 </html>
--- src/article/articles.java
+++ src/article/articles.java
@@ -1,5 +1,11 @@
 package article;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,38 +15,47 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
-import static java.lang.System.out;
-
 @WebServlet(name = "articles")
 public class articles extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //设置网页响应类型
-        response.setContentType("text/html");
+        response.setContentType("text/html;charset=utf-8");
         //实现具体操作
         PrintWriter out = response.getWriter();
-        out.println("This is a new servlet page "+request.getQueryString());
+        //out.println("This is a new servlet page "+request.getQueryString());
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/articles", "root", "82870808qyy");
             Statement stmt=conn.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT content FROM articleNames");
+            ResultSet rs=stmt.executeQuery("SELECT * FROM articleNames");
 
+            JsonArray json=new JsonArray();
             while(rs.next()){
-                out.println(rs.getString("content"));
+                JsonObject data=new JsonObject();
+                data.addProperty("name", rs.getString("name"));
+                data.addProperty("theme", rs.getString("theme"));
+                data.addProperty("time", rs.getDate("time").toString());
+                data.addProperty("content", rs.getString("content"));
+                json.add(data);
             }
+            out.println(json.toString());
 
             rs.close();
             stmt.close();
             conn.close();
+
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
