a2b6f12bbbd24c178b3e2bdcb8a3edac2b57d56b
zpvoh <16302010023@fudan.edu.cn>
2018-08-18 18:28:32 +0800

    article section complete

--- src/article/FetchArticle.java
+++ src/article/FetchArticle.java
@@ -1,10 +1,7 @@
 package article;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,37 +12,36 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
-@WebServlet(name = "articles")
-public class articles extends HttpServlet {
+@WebServlet(name = "FetchArticle")
+public class FetchArticle extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //设置网页响应类型
         response.setContentType("text/html;charset=utf-8");
         //实现具体操作
         PrintWriter out = response.getWriter();
         //out.println("This is a new servlet page "+request.getQueryString());
-        String name=request.getParameter("name");
-        if(!name.equals("Zpvoh") && !name.equals("zpvoh")){
+        String name = request.getParameter("name");
+        if (!name.equals("Zpvoh") && !name.equals("zpvoh")) {
             return;
         }
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/articles", "root", "82870808qyy");
-            Statement stmt=conn.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT * FROM articles");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/articles", "root", "82870808qyy");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM articles WHERE id=" + request.getParameter("id"));
+            rs.next();
 
-            JsonArray json=new JsonArray();
-            while(rs.next()){
-                JsonObject data=new JsonObject();
-                data.addProperty("id", rs.getString("id"));
-                data.addProperty("title", rs.getString("title"));
-                data.addProperty("theme", rs.getString("theme"));
-                data.addProperty("description", rs.getString("description"));
-                data.addProperty("cover_img", rs.getString("cover_img"));
-                data.addProperty("time", rs.getDate("time").toString());
-                data.addProperty("content", rs.getString("content"));
-                json.add(data);
-            }
+            JsonArray json = new JsonArray();
+            JsonObject data = new JsonObject();
+            data.addProperty("id", rs.getString("id"));
+            data.addProperty("title", rs.getString("title"));
+            data.addProperty("theme", rs.getString("theme"));
+            data.addProperty("description", rs.getString("description"));
+            data.addProperty("cover_img", rs.getString("cover_img"));
+            data.addProperty("time", rs.getDate("time").toString());
+            data.addProperty("content", rs.getString("content"));
+            json.add(data);
             out.println(json.toString());
 
             rs.close();
@@ -58,8 +54,6 @@ public class articles extends HttpServlet {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web/index.jsp
+++ web/index.jsp
@@ -6,19 +6,27 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>$Title$</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+    <title>Gloomy</title>
+    <link rel="icon" href="icon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.0/mini-default.min.css">
+    <link rel="stylesheet" href="css/index.css">
     <script src="https://cdn.bootcss.com/react/16.4.0/umd/react.development.js"></script>
     <script src="https://cdn.bootcss.com/react-dom/16.4.0/umd/react-dom.development.js"></script>
     <script src="https://cdn.bootcss.com/babel-standalone/6.26.0/babel.min.js"></script>
     <script src="https://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/handleCookie.js"></script>
     <script type="text/babel" src="js/react_render.js"></script>
     <script src="js/getContent.js"></script>
 </head>
 <body>
+<div id="dialog"></div>
+<header id="head" class="sticky"></header>
+<div id="page"></div>
+<footer id="foot" class="sticky"></footer>
 
 </body>
 </html>
