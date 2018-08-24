e9cf94ec7c67ec5ae2a1a988d1f5641fb0e15a3c
zpvoh <16302010023@fudan.edu.cn>
2018-08-24 21:41:55 +0800

    new environment deploy

--- src/article/FetchArticle.java
+++ src/article/FetchArticle.java
@@ -21,6 +21,8 @@ public class FetchArticle extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
         //实现具体操作
         PrintWriter out = response.getWriter();
+        InfoReader reader=new InfoReader();
+        out.println(reader.path);
         //out.println("This is a new servlet page "+request.getQueryString());
         String name = request.getParameter("name");
 
@@ -28,9 +30,6 @@ public class FetchArticle extends HttpServlet {
             out.print("invalid");
             return;
         }
-
-        InfoReader reader=new InfoReader();
-        out.println(reader.path);
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
